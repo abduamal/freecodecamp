905d49eafb13b4d765f1d9c1665b29e1ea1469cd
Amal Abdu <abduamal@ymail.com>
2021-12-14 17:13:13 -0600

    self-closing JSX tags

--- React/JSX.js
+++ React/JSX.js
@@ -1,14 +1,18 @@
 // Boiler:
-  const JSX = (
-    <div>
-      <h1>Add a class to this div</h1>
-    </div>
-  );
+// const JSX = (
+//   <div>
+//     <h2>Welcome to React!</h2> <br >
+//       <p>Be sure to close all tags!</p>
+//       <hr >
+//       </div>
+//       );
 // Challenge:
   // Apply a class of myDiv to the div provided in the JSX code.
 // Solution:
   const JSX = (
-    <div className='myDiv'>
-      <h1>Add a class to this div</h1>
+    <div>
+      <h2>Welcome to React!</h2> <br />
+      <p>Be sure to close all tags!</p>
+      <br />
     </div>
   );
