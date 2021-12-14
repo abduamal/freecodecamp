070204d35b544cdaf199329ba695eb3d01bb4812
Amal Abdu <abduamal@ymail.com>
2021-12-14 11:10:52 -0600

    refactor complex JSX element solution

--- React/JSX.js
+++ React/JSX.js
@@ -4,13 +4,14 @@
   // Define a new constant JSX that renders a div which contains the following elements in order:
   // An h1, a p, and an unordered list that contains three li items. You can include any text you want within each element.
 // Solution:
-const JSX =
-<div>
-  <h1>Something</h1>
-  <p>Something Else</p>
-  <ul>
-    <li>one</li>
-    <li>two</li>
-    <li>three</li>
-  </ul>
-</div>
+const JSX = (
+  <div>
+    <h1>Something</h1>
+    <p>Something Else</p>
+    <ul>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
+  </div>
+);
