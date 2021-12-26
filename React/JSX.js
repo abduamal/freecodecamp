cfc2338a6b27b815fbfcecca3d9f695533c1f702
Amal Abdu <abduamal@ymail.com>
2021-12-25 20:51:28 -0600

    props..just boiler

--- React/JSX.js
+++ React/JSX.js
@@ -1,39 +1,32 @@
 // Boiler:
-class TypesOfFood extends React.Component {
+const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+      <p>The current date is: </p>
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h1>Types of Food:</h1>
-        {/* Change code below this line */}
-
-        {/* Change code above this line */}
+        <h3>What date is it?</h3>
+        { /* Change code below this line */ }
+        <CurrentDate />
+        { /* Change code above this line */ }
       </div>
     );
   }
 };
-
-// Change code below this line
 
 // Challenge:
   //Define a class MyComponent that extends React.Component.
   //Its render method should return a div that contains an h1 tag with the text: My First React Component! in it.
 // Solution:
 // Change code below this line
-
-class MyComponent extends React.Component {
-  constructor(props){
-    super(props)
-  }
-  render() {
-    return(
-      <div>
-      <h1>My First React Component!</h1>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'))
