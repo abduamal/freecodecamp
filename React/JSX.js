b468991f20c996d0d708ae7105f775d452670a5e
Amal Abdu <abduamal@ymail.com>
2021-12-28 12:39:44 -0600

    nesting for loops

--- React/JSX.js
+++ React/JSX.js
@@ -1,12 +1,20 @@
 // Boiler:
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
 
-const myArr = [2, 3, 4, 5, 6];
+  // Only change code above this line
+  return product;
+}
 
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
 // Challenge:
-  // Declare and initialize a variable total to 0.
-  // Use a for loop to add the value of each element of the myArr array to total.
-  let total = 0
-  for (let i = 0; i < myArr.length; i++){
-    total += myArr[i]
+  // Modify function multiplyAll so that it returns the product of all the
+  // numbers in the sub-arrays of arr.
+// Solution:
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j]
+    }
   }
