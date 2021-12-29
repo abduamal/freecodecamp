99f9f4bb029ab6cf2b9546ed233ab865b1309402
Amal Abdu <abduamal@ymail.com>
2021-12-29 12:48:23 -0600

    generate random fractions with JavaScript

--- React/JSX.js
+++ React/JSX.js
@@ -1,57 +1,15 @@
 // Boiler:
+function randomFraction() {
 
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
-
-function lookUpProfile(name, prop) {
   // Only change code below this line
+
+  return 0
 
   // Only change code above this line
 }
 
-lookUpProfile("Akira", "likes");
-
 // Challenge:
-  //The function should check if name is an actual contact's firstName and the given
-  // property (prop) is a property of that contact.
-  //If both are true, then return the "value" of that property.
-  //If name does not correspond to any contacts then return the string No such contact.
-  //If prop does not correspond to any valid properties of a contact found to match name
-  // then return the string No such property.
-
+  //Change randomFraction to return a random number instead of returning 0.
 
 // Solution:
-for (let i = 0; i < contacts.length; i++) {
-  if (contacts[i].firstName === name) {
-    if (prop in contacts[i]) {
-      return contacts[i][prop];
-    } else {
-      return "No such property";
-    }
-  }
-}
-return "No such contact";
+return Math.random()
