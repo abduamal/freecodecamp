cdee369c1caa8a73868a658a7be33d3a1d22c60b
Amal Abdu <abduamal@ymail.com>
2021-12-31 09:44:49 -0600

    pass an array as props

--- React/JSX.js
+++ React/JSX.js
@@ -1,45 +1,48 @@
 // Boiler:
-const CurrentDate = (props) => {
-  return (
-    <div>
-      { /* Change code below this line */ }
-      <p>The current date is: </p>
-      { /* Change code above this line */ }
-    </div>
-  );
+const List = (props) => {
+  { /* Change code below this line */ }
+  return <p>{}</p>
+  { /* Change code above this line */ }
 };
 
-class Calendar extends React.Component {
+class ToDo extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h3>What date is it?</h3>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
         { /* Change code below this line */ }
-        <CurrentDate />
+        <List/>
+        <h2>Tomorrow</h2>
+        <List/>
         { /* Change code above this line */ }
       </div>
     );
   }
 };
 // Challenge:
-  //There are Calendar and CurrentDate components in the code editor.
-  //When rendering CurrentDate from the Calendar component, pass in a property
-  // of date assigned to the current date from JavaScript's Date object.
-  //Then access this prop in the CurrentDate component, showing its value within
-  // the p tags.
+  //There are List and ToDo components in the code editor. When rendering each
+   // List from the ToDo component, pass in a tasks property assigned to an
+   // array of to-do tasks, for example ["walk dog", "workout"].
+  //Then access this tasks array in the List component, showing its value
+  // within the p element. Use join(", ") to display the props.tasksarray
+  // in the p element as a comma separated list.
+  //Today's list should have at least 2 tasks and tomorrow's should have at least 3 tasks.
 
 // Solution:
+{ /* Change code below this line */ }
+return <p>{props.tasks.join(', ')}</p>
+{ /* Change code above this line */ }
+};
 <div>
+  <h1>To Do Lists</h1>
+  <h2>Today</h2>
   { /* Change code below this line */ }
-  <p>The current date is: {props.date}</p>
-  { /* Change code above this line */ }
-</div>
-<div>
-  <h3>What date is it?</h3>
-  { /* Change code below this line */ }
-  <CurrentDate date={Date()}/>
+  <List tasks={["walk dog", "workout"]} />
+  <h2>Tomorrow</h2>
+  <List tasks={["walk dog", "workout", "play"]} />
   { /* Change code above this line */ }
 </div>
