94e418ffd90536f6d5a0d973e57a6b488828e6ea
Amal Abdu <abduamal@ymail.com>
2022-02-10 12:19:08 -0600

    render state in the user interface

--- React/JSX.js
+++ React/JSX.js
@@ -1,35 +1,28 @@
 // Boiler:
 
-class CampSite extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
   }
   render() {
     return (
       <div>
-        <Camper />
+        { /* Change code below this line */ }
+
+        { /* Change code above this line */ }
       </div>
     );
   }
 };
-// Change code below this line
 
 
 // Challenge:
-  //Define the Camper component and assign it default props of { name: 'CamperBot' }.
-  //Inside the Camper component, render any code that you want, but make sure to have
-    // one p element that includes only the name value that is passed in as a prop.
-  //Finally, define propTypes on the Camper component to require name to be provided
-    // as a prop and verify that it is of type string.
+  // Define an h1 tag in the component's render method which renders the value
+   // of name from the component's state.
 
 // Solution:
 
-const Camper = props => <p>{props.name}</p>;
-
-Camper.defaultProps = {
-  name: "CamperBot"
-};
-
-Camper.propTypes = {
-  name: PropTypes.string.isRequired
-};
+<h1>{this.state.name}</h1>
